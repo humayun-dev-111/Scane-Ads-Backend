5590f246ac02f5a879a1a2edaf987854bad551ee
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-05-26 15:23:08 +0600

    Initial Setup added and prisma schema added

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import status from "http-status";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
+import { PaymentController } from "./app/modules/Payment/Payment.controller";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      "https://scne-ads.netlify.app",
+      "https://scene-ads.netlify.app",
+      "*",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(
+  "/api/payment/webhook",
+  express.raw({ type: "application/json" }),
+  PaymentController.stripeWebhook
+);
+
+app.use(express.json({ limit: "5000mb" })); // increase JSON body limit
+app.use(express.urlencoded({ limit: "5000mb", extended: true })); // increase form body limit
+
+app.use("/api", router);
+
+app.use(globalErrorHandler);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Scne Ads Server is running 🎉🎉");
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(status.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found",
+    },
+  });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import { Server as HTTPServer } from "http";
+import app from "./app";
+import prisma from "./shared/prisma";
+import { ORGANISATION_ROLE, USER_ROLE } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { setupWebSocket } from "./utils/websocket";
+import config from "./config";
+import { startCampaignStatusUpdater } from "./corn/campaignStatusUpdater";
+import 'dotenv/config';
+
+// import cron from "node-cron";
+// import axios from "axios";
+const port = 3000;
+
+async function ensureAdmin() {
+  const existingAdmin = await prisma.user.findFirst({
+    where: { email: "humayun506034@gmail.com" },
+  });
+
+  if (!existingAdmin) {
+    const hashedPassword = await bcrypt.hash("123456", 12); // default password
+    await prisma.user.create({
+      data: {
+        first_name: "Mr.",
+        last_name: "Admin",
+        phone: "+8801747477746",
+        email: "humayun506034@gmail.com",
+        image: "https://res.cloudinary.com/dbqjujtbq/image/upload/v1771408116/attachments/media/Image-1771408115399.jpg",
+        password: hashedPassword,
+        organisation_name: "SCNE Ads",
+        role: USER_ROLE.admin,
+        organisation_role: ORGANISATION_ROLE.advertiser,
+        is_verified: true,
+        status: "active",
+      },
+    });
+    console.log(
+      "✅ Default Admin created (email: admin@scneads.com, password: 123456)"
+    );
+  } else {
+    console.log("ℹ️ Admin already exists, skipping creation.");
+  }
+}
+
+async function main() {
+  // Ensure default admin exists first
+  await ensureAdmin();
+  startCampaignStatusUpdater();
+
+  const httpServer: HTTPServer = app.listen(port, () => {
+    console.log("🚀 Server is running on port", port);
+  });
+
+  const { wss, onlineUsers } = setupWebSocket(
+    httpServer,
+    config.jwt.access_token_secret as string
+  );
+  app.set("wss", wss);
+  app.set("onlineUsers", onlineUsers);
+}
+
+main().catch((err) => {
+  console.error("❌ Server failed to start:", err);
+  process.exit(1);
+});
